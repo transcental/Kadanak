a645f5e1fd6ea6554a0aba5e0ce5249b74beac4b
Dillon Barnes <dillonb07dev@gmail.com>
2024-05-27 16:47:56 +0100

    add contact section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Work from "./components/Work/Work";
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Work />
+      <Contact />
     </>
   );
 }
---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,38 @@
+import styles from "./Contact.module.css";
+
+export default function Contact() {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.heading}>Contact</h2>
+      <ul className={styles.buttons}>
+        <li>
+          <a
+            href="https://x.com"
+            className={`${styles.x} ${styles.contact_item}`}
+          >
+            <img src="/x.svg" alt="X logo" />
+            @KADanak
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://instagram.com"
+            className={`${styles.instagram} ${styles.contact_item}`}
+          >
+            <img src="/instagram.svg" alt="Instagram logo" />
+            @kadanak
+          </a>
+        </li>
+        <li>
+          <a
+            href="mailto:kadanak@dillonb07.studio"
+            className={`${styles.email} ${styles.contact_item}`}
+          >
+            <img src="/mail.svg" alt="Email icon" />
+            hi@kadanak.co.uk
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
