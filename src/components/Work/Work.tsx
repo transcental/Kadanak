bee4dbf0587b457f226084f13545de69e845f7fe
Dillon Barnes <dillonb07dev@gmail.com>
2024-05-27 16:18:53 +0100

    add huds to work section

--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -47,12 +47,69 @@ function Posters() {
   );
 }
 
+function HUDs() {
+  return (
+    <div className={styles.hud_container}>
+      <div className={styles.hud}>
+        <div className={styles.hud_info}>
+          <h4>HUD Title</h4>
+          <p>
+            Mollit quis sit id nisi aliquip. Deserunt nisi aliquip commodo
+            ipsum. Reprehenderit sunt ut mollit eu aliqua. Eiusmod duis dolore
+            tempor non tempor ea fugiat. Culpa sit elit velit adipisicing Lorem
+            ipsum sunt et.
+          </p>
+        </div>
+        <img src="https://via.placeholder.com/370x262" alt="hud" />
+      </div>
+      <div className={styles.hud}>
+        <img src="https://via.placeholder.com/370x262" alt="hud" />
+        <div className={styles.hud_info}>
+          <h4>HUD Title</h4>
+          <p>
+            Mollit quis sit id nisi aliquip. Deserunt nisi aliquip commodo
+            ipsum. Reprehenderit sunt ut mollit eu aliqua. Eiusmod duis dolore
+            tempor non tempor ea fugiat. Culpa sit elit velit adipisicing Lorem
+            ipsum sunt et.
+          </p>
+        </div>
+      </div>
+      <div className={styles.hud}>
+        <div className={styles.hud_info}>
+          <h4>HUD Title</h4>
+          <p>
+            Mollit quis sit id nisi aliquip. Deserunt nisi aliquip commodo
+            ipsum. Reprehenderit sunt ut mollit eu aliqua. Eiusmod duis dolore
+            tempor non tempor ea fugiat. Culpa sit elit velit adipisicing Lorem
+            ipsum sunt et.
+          </p>
+        </div>
+        <img src="https://via.placeholder.com/370x262" alt="hud" />
+      </div>
+      <div className={styles.hud}>
+        <img src="https://via.placeholder.com/370x262" alt="hud" />
+        <div className={styles.hud_info}>
+          <h4>HUD Title</h4>
+          <p>
+            Mollit quis sit id nisi aliquip. Deserunt nisi aliquip commodo
+            ipsum. Reprehenderit sunt ut mollit eu aliqua. Eiusmod duis dolore
+            tempor non tempor ea fugiat. Culpa sit elit velit adipisicing Lorem
+            ipsum sunt et.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function Work() {
   return (
     <div className={styles.container}>
       <h2 className={styles.heading}>Work</h2>
       <h3 className={styles.subheading}>Posters</h3>
       <Posters />
+      <h3 className={styles.subheading}>Heads Up Displays</h3>
+      <HUDs />
     </div>
   );
 }
