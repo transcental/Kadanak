7314c6b947be3e74e6b2b12d217472576a43bdc4
Dillon Barnes <dillonb07dev@gmail.com>
2024-05-27 15:20:33 +0100

    add hero standin

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,9 @@
+import styles from "./Hero.module.css";
+
+export default function Hero() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.text_wrapper}>FLEEB</h1>
+    </div>
+  );
+}
